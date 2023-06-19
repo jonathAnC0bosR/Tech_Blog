6806cc676a5bdbc232c0de7d7ca012276c7e9253
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-06-19 03:26:04 -0600

    Styled the main handlebars page with bootstrap

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,51 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    // const userData = await User.findAll().catch((err) => {
-    //     res.json(err);
-    // });
-
-    // const users = userData.map((user) => user.get({ plain: true}));
     try {
-        res.render('homepage');
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User, 
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+        console.log(posts);
+        res.render('homepage', {
+            posts, 
+            logged_in: req.session.logged_in
+        });
+
+
     } catch (err) {
         res.status(500).json(err);
-        return;
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        
+        const postData = await Post.findByPk.apply(req.params.id, {
+            include: [
+                {
+                    model: User, 
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true});
+        res.render('post', {
+            ...post, 
+            logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
     }
 })
 
--- models/index.js
+++ models/index.js
@@ -3,6 +3,11 @@ const Post = require('./Post');
 
 User.hasMany(Post, {
     foreignKey: 'user_username',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_username'
 });
 
 module.exports = { User, Post };
